ff35c9874a5ac10cd44f835f390a44f4e88d582a
aptalca <thedjinn@yahoo.com>
2015-04-28 18:53:00 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-if [ ! -d /config/config ]; then
-    mkdir -p /config/config
-    cp /etc/boinc-client/* /config/config/
-    chmod -R a+rw /config/config
-fi
-
-if [ ! -d /config/data ]; then
-    mkdir -p /config/data
-    cp /var/lib/boinc-client/* /config/data/
-    chmod -R a+rw /config/data
-fi
-
-echo -e "boinc\nboinc" | passwd boinc
-rm -r /etc/boinc-client
-ln -s /config/config /etc/boinc-client
-rm -r /var/lib/boinc-client
-ln -s /config/data /var/lib/boinc-client
+cp /root/boinc/boinc_7.2.42_x86_64-pc-linux-gnu.sh /config/boinc_7.2.42_x86_64-pc-linux-gnu.sh
+chmod +x /config/boinc_7.2.42_x86_64-pc-linux-gnu.sh
+/config/boinc_7.2.42_x86_64-pc-linux-gnu.sh
+echo -e "boinc\nboinc" | passwd root
 service xrdp restart
-service boinc-client restart
