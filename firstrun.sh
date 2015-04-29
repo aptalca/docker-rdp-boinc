7d0c48e868f81220c5af08b469bcee041752cedc
aptalca <thedjinn@yahoo.com>
2015-04-28 20:23:35 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -2,6 +2,7 @@
 
 cp /root/boinc/boinc_7.2.42_x86_64-pc-linux-gnu.sh /config/boinc_7.2.42_x86_64-pc-linux-gnu.sh
 chmod +x /config/boinc_7.2.42_x86_64-pc-linux-gnu.sh
-sh /config/boinc_7.2.42_x86_64-pc-linux-gnu.sh
+cd /config
+./boinc_7.2.42_x86_64-pc-linux-gnu.sh
 echo -e "boinc\nboinc" | passwd root
 service xrdp restart
