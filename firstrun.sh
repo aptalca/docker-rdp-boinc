46c74ef94f1e235dc0a3cc3aa85c4d1c56dc239e
aptalca <thedjinn@yahoo.com>
2015-05-01 13:13:29 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Install boinc client and manager
-cp /root/nobody/boinc/boinc_7.2.42_x86_64-pc-linux-gnu.sh /config/boinc_7.2.42_x86_64-pc-linux-gnu.sh
+cp /nobody/boinc/boinc_7.2.42_x86_64-pc-linux-gnu.sh /config/boinc_7.2.42_x86_64-pc-linux-gnu.sh
 chmod +x /config/boinc_7.2.42_x86_64-pc-linux-gnu.sh
 chown -R nobody:users /config
-cd /config
 su -c "cd /config && ./boinc_7.2.42_x86_64-pc-linux-gnu.sh" -s /bin/sh nobody
 su -c "/config/BOINC/run_client --daemon" -s /bin/sh nobody
