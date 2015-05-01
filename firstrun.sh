0eec06d95bd7ebb07dee7bab7fc387a37a4934e3
aptalca <thedjinn@yahoo.com>
2015-05-01 15:34:47 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -4,6 +4,5 @@
 cp /nobody/boinc/boinc_7.2.42_x86_64-pc-linux-gnu.sh /config/boinc_7.2.42_x86_64-pc-linux-gnu.sh
 chmod +x /config/boinc_7.2.42_x86_64-pc-linux-gnu.sh
 chown -R nobody:users /config
-apt-get update && apt-get install -y libxss1
 su -c "cd /config && ./boinc_7.2.42_x86_64-pc-linux-gnu.sh" -s /bin/sh nobody
 su -c "/config/BOINC/run_client --daemon" -s /bin/sh nobody
